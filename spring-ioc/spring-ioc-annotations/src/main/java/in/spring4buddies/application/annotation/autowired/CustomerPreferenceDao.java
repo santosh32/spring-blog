2528c528ad22d0d5f4bc3df2cd17af89c928bb6c
Santosh Spring32 <santosh.spring32@gmail.com>
2015-11-08 08:56:37 +0530

    spring-ioc-annatotion : autowired api added

---
+++ spring-ioc/spring-ioc-annotations/src/main/java/in/spring4buddies/application/annotation/autowired/CustomerPreferenceDao.java
@@ -0,0 +1,8 @@
+package in.spring4buddies.application.annotation.autowired;
+
+public class CustomerPreferenceDao {
+
+	public String getCustomerPreference() {
+		return "This is response getCustomerPreference()";
+	}
+}
---
+++ spring-ioc/spring-ioc-annotations/src/main/java/in/spring4buddies/application/annotation/autowired/MovieCatalog.java
@@ -0,0 +1,9 @@
+package in.spring4buddies.application.annotation.autowired;
+
+public class MovieCatalog {
+
+	public String getMovieCatalog() {
+		return "response from MovieCatalog class";
+	}
+
+}
