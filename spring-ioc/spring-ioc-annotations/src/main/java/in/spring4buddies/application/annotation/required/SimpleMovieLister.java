c7a1fddce43dfc55b569ec5936e0c31d30a9a1af
Santosh Spring32 <santosh.spring32@gmail.com>
2015-11-08 08:19:16 +0530

    spring-ioc-annatotion : autowired api added

--- spring-ioc/spring-ioc-annotations/src/main/java/in/spring4buddies/application/annotation/required/SimpleMovieLister.java
+++ spring-ioc/spring-ioc-annotations/src/main/java/in/spring4buddies/application/annotation/required/SimpleMovieLister.java
@@ -4,18 +4,14 @@ import org.springframework.beans.factory.annotation.Required;
 
 public class SimpleMovieLister {
 
-	private MovieFinder movieFinder;
+	private MovieFinder movieFinderRequired;
 
 	@Required
-	public void setMovieFinder(MovieFinder movieFinder) {
-		this.movieFinder = movieFinder;
-	}
-
 	public String getMovieByUps(String ups) {
-		return movieFinder.getMovieByUps(ups);
+		return movieFinderRequired.getMovieByUps(ups);
 	}
 
-	public MovieFinder getMovieFinder() {
-		return movieFinder;
+	public void setMovieFinderRequired(MovieFinder movieFinderRequired) {
+		this.movieFinderRequired = movieFinderRequired;
 	}
 }
