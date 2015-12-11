4b592f2fba30eb99852af5add4a7fb79f1959323
Santosh Spring32 <santosh.spring32@gmail.com>
2015-12-11 18:51:45 +0530

    spring-mvc-file-upload-config : added

---
+++ spring-mvc/spring-mvc-file-upload-config/src/main/java/in/spring4buddies/application/validator/MultiFileValidator.java
@@ -0,0 +1,33 @@
+package in.spring4buddies.application.validator;
+
+import in.spring4buddies.application.model.FileBucket;
+import in.spring4buddies.application.model.MultiFileBucket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class MultiFileValidator implements Validator {
+
+	public boolean supports(Class<?> clazz) {
+		return MultiFileBucket.class.isAssignableFrom(clazz);
+	}
+
+	public void validate(Object obj, Errors errors) {
+		MultiFileBucket multiBucket = (MultiFileBucket) obj;
+
+		int index = 0;
+
+		for (FileBucket file : multiBucket.getFiles()) {
+			if (file.getFile() != null) {
+				if (file.getFile().getSize() == 0) {
+					errors.rejectValue("files[" + index + "].file",
+							"missing.file");
+				}
+			}
+			index++;
+		}
+
+	}
+}
