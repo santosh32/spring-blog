4a590111260a8fa8d1765a119da007a0f7c12546
Santosh Spring32 <santosh.spring32@gmail.com>
2016-03-14 12:02:51 +0530

    added/modified projects

---
+++ app/sample-persistence-services/src/main/java/in/spring4buddies/application/common/PdfHeader.java
@@ -0,0 +1,7 @@
+package in.spring4buddies.application.common;
+
+public interface PdfHeader {
+	String[] FIRST_ROW = new String[] { "Emp. ID", "Department", "PF. No" };
+	String[] SECOND_ROW = new String[] { "Name", "Bank", "PAN" };
+	String[] THIRD_ROW = new String[] { "Designation", "Bank A/c No", "Paid Days" };
+}
