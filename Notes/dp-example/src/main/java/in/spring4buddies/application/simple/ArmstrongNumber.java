69aedfae2e083a2a8d6f06385aec28f5c76a31bd
Santosh Spring32 <santosh.spring32@gmail.com>
2016-06-23 12:35:54 +0530

    added pyramids

--- Notes/dp-example/src/main/java/in/spring4buddies/application/simple/ArmstrongNumber.java
+++ Notes/dp-example/src/main/java/in/spring4buddies/application/simple/ArmstrongNumber.java
@@ -3,9 +3,22 @@ package in.spring4buddies.application.simple;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
+		int number = 153;
+		System.out.println(number + " isArmstrong - " + isArmstrong(number));
 	}
 
 	private static boolean isArmstrong(int number) {
-		return true;
+		int temp = number;
+		int armstrong = 0;
+		int reminder = 0;
+		while (number > 0) {
+			reminder = number % 10;
+			number = number / 10;
+			armstrong = armstrong + reminder * reminder * reminder;
+		}
+		if (armstrong == temp) {
+			return true;
+		}
+		return false;
 	}
 }
