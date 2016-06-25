ce68d4e773103b71589627316770562542e18eb4
Santosh Spring <santosh.spring32@gmail.com>
2016-06-25 21:38:59 +0530

    sf-securities added

---
+++ Notes/sf-security-example/sf-security-default-login-form/src/main/java/in/spring4buddies/application/configuration/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package in.spring4buddies.application.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("user").password("password")
+				.roles("USER");
+		auth.inMemoryAuthentication()
+				.withUser("admin").password("password")
+				.roles("ADMIN");
+		auth.inMemoryAuthentication()
+				.withUser("dba").password("password")
+				.roles("ADMIN", "DBA");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/home").permitAll()
+			.antMatchers("/admin/**").access("hasRole('ADMIN')")
+			.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+			.and()
+				.formLogin()
+			.and()
+				.exceptionHandling().accessDeniedPage("/Access_Denied");
+	}
+}
