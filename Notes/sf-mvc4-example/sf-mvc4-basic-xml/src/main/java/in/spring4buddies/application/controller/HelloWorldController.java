617464a0040d3a6d341c94e3fb38470fec96709b
Santosh Spring <santosh.spring32@gmail.com>
2016-07-02 20:10:15 +0530

    added sf-mvcies

---
+++ Notes/sf-mvc4-example/sf-mvc4-basic-xml/src/main/java/in/spring4buddies/application/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package in.spring4buddies.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String hello(ModelMap model) {
+		model.addAttribute("name", "SF Hello World!");
+		return "helloWorld";
+	}
+}
