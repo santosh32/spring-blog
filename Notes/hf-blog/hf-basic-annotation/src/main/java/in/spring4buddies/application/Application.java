f71fd2492ab4d78283c214cba32c49b7902c5f8c
Santosh Spring32 <santosh.spring32@gmail.com>
2016-07-19 19:00:58 +0530

    hf added

--- Notes/hf-blog/hf-basic-annotation/src/main/java/in/spring4buddies/application/Application.java
+++ Notes/hf-blog/hf-basic-annotation/src/main/java/in/spring4buddies/application/Application.java
@@ -1,7 +1,48 @@
 package in.spring4buddies.application;
 
+import in.spring4buddies.application.config.HibernateUtil;
+
+import org.hibernate.Session;
+
 public class Application {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
+		save();
+		update();
+//		saveOrUpdate();
+	}
+
+	public static void save() {
+		System.out.println("==============================");
+		System.out.println("executing the save() ");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Student student = new Student(1, 22, "Ram");
+		session.beginTransaction();
+		long id = (long) session.save(student);
+		System.out.println("Generated Identifier:" + id);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	public static void saveOrUpdate() {
+		System.out.println("==============================");
+		System.out.println("executing the saveOrUpdate() ");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Student s = new Student(1, 25, "Ram");
+		session.beginTransaction();
+		session.saveOrUpdate(s);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	public static void update() {
+		System.out.println("==============================");
+		System.out.println("executing the update() ");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Student s = new Student(1, 22, "Mahesh");
+		session.beginTransaction();
+		session.update(s);
+		session.getTransaction().commit();
+		session.close();
 	}
 }
---
+++ Notes/hf-blog/hf-basic-annotation/src/main/java/in/spring4buddies/application/Student.java
@@ -0,0 +1,55 @@
+package in.spring4buddies.application;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "student")
+public class Student {
+	
+	@Id
+	@Column(name = "id")
+	private long id;
+	
+	@Column(name = "age")
+	private int age;
+	
+	@Column(name = "name")
+	private String name;
+
+	public Student() {
+	}
+
+	public Student(int id, int age, String name) {
+		this.id = id;
+		this.age = age;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
