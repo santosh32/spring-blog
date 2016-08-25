dec54f994159ec644968521167311707ec51a316
Santosh Spring32 <santosh.spring32@gmail.com>
2016-08-25 19:35:28 +0530

    rest client resttemplate added

--- Notes/spring-boot/spring-boot-rest-client-resttemplate/src/main/java/in/spring4buddies/application/rest/RestClientApplication.java
+++ Notes/spring-boot/spring-boot-rest-client-resttemplate/src/main/java/in/spring4buddies/application/rest/RestClientApplication.java
@@ -1,15 +1,23 @@
 package in.spring4buddies.application.rest;
 
+import in.spring4buddies.application.rest.model.Book;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class RestBootApplication {
+public class RestClientApplication {
+	
+	private static String URI = "http://localhost:8080/book";
 
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(
-				RestBootApplication.class, args);
+				RestClientApplication.class, args);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		Book book = restTemplate.getForObject(URI, Book.class, "1000");
 	}
 
 }
