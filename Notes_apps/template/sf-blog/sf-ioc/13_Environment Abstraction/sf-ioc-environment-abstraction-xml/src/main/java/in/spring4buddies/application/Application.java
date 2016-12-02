4f23166194414a5ce07af106d66a9c5744a83244
Spring32 <spring4buddies@gmail.com>
2016-12-02 19:17:54 +0530

    sf-ioc-environment/propeties project structure prepared
    Change-Id: I84c63cb75930015921be29e4d953a5d72d103b9d

--- Notes_apps/template/sf-blog/sf-ioc/13_Environment Abstraction/sf-ioc-environment-abstraction-xml/src/main/java/in/spring4buddies/application/Application.java
+++ Notes_apps/template/sf-blog/sf-ioc/13_Environment Abstraction/sf-ioc-environment-abstraction-xml/src/main/java/in/spring4buddies/application/Application.java
@@ -1,8 +1,15 @@
 package in.spring4buddies.application;
 
-public class Application { // implements CommandLineRunner {
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
 
 	public static void main(String[] args) throws Exception {
+
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+		((AbstractApplicationContext) context).close();
 
 	}
 }
