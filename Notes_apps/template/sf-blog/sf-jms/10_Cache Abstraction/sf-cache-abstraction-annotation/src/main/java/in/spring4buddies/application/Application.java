e63da30eec07f673a1b67e97fc40ebf5debd6e51
Santosh Spring <santosh.spring32@gmail.com>
2016-12-04 19:47:21 +0530

    sf-cache-annotation : refactoring but not working
    Change-Id: I2243d89989e29edb0ae06cd73fcfe4395a5420ea

--- Notes_apps/template/sf-blog/sf-jms/10_Cache Abstraction/sf-cache-abstraction-annotation/src/main/java/in/spring4buddies/application/Application.java
+++ Notes_apps/template/sf-blog/sf-jms/10_Cache Abstraction/sf-cache-abstraction-annotation/src/main/java/in/spring4buddies/application/Application.java
@@ -1,20 +1,20 @@
 package in.spring4buddies.application;
 
-import in.spring4buddies.application.service.ProductService;
+import in.spring4buddies.application.service.ProductServiceImpl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		AbstractApplicationContext context = new AnnotationConfigApplicationContext(BuddiesCacheConfig.class);
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-		ProductService service = (ProductService) context.getBean("productService");
+		ProductServiceImpl service =  context.getBean(ProductServiceImpl.class);
 
 		logger.info("IPhone ->" + service.getByName("IPhone"));
 		logger.info("IPhone ->" + service.getByName("IPhone"));
