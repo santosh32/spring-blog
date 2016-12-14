5f65783a769a1136dddd162b2ff36d665123a66e
Spring32 <spring4buddies@gmail.com>
2016-12-14 17:39:21 +0530

    sf-jmstemplate-config : modified
    Change-Id: I0874911fdf94acd81e577acd8c05dfee44da9800

--- Notes_apps/template/sf-blog/sf-jms/2_JmsTemplate/sf-jmstemplate-config/src/main/java/in/spring4buddies/application/config/AppConfig.java
+++ Notes_apps/template/sf-blog/sf-jms/2_JmsTemplate/sf-jmstemplate-config/src/main/java/in/spring4buddies/application/config/AppConfig.java
@@ -5,37 +5,54 @@ import in.spring4buddies.application.jms.JmsProducer;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @PropertySource("activemq.properties")
 @ComponentScan(basePackages = "in.spring4buddies.application.*")
 public class AppConfig {
+	
+	@Autowired
+	private Environment environment;
 
 	@Bean
 	public ActiveMQConnectionFactory activeMQConnectionFactory() {
 		ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+		activeMQConnectionFactory.setBrokerURL(environment.getProperty("broker.url"));
 		return activeMQConnectionFactory;
 	}
 
 	@Bean
 	public ActiveMQQueue activeMQQueue() {
-		ActiveMQQueue activeMQQueue = new ActiveMQQueue();
+		ActiveMQQueue activeMQQueue = new ActiveMQQueue(environment.getProperty("cutomer.queue"));
 		return activeMQQueue;
 	}
 
 	@Bean
-	public JmsProducer jmsProducer() {
+	public JmsTemplate jmsTemplate(ActiveMQConnectionFactory activeMQConnectionFactory, ActiveMQQueue activeMQQueue) {
+		JmsTemplate jmsTemplate = new JmsTemplate();
+		jmsTemplate.setConnectionFactory(activeMQConnectionFactory);
+		jmsTemplate.setDefaultDestination(activeMQQueue);
+		return jmsTemplate;
+	}
+
+	@Bean
+	public JmsProducer jmsProducer(JmsTemplate jmsTemplate) {
 		JmsProducer jmsProducer = new JmsProducer();
+		jmsProducer.setJmsTemplate(jmsTemplate);
 		return jmsProducer;
 	}
 
 	@Bean
-	public JmsConsumer jmsConsumer() {
+	public JmsConsumer jmsConsumer(JmsTemplate jmsTemplate) {
 		JmsConsumer jmsConsumer = new JmsConsumer();
+		jmsConsumer.setJmsTemplate(jmsTemplate);
 		return jmsConsumer;
 	}
 }
