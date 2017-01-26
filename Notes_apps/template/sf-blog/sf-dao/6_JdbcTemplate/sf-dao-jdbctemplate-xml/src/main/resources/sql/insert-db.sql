f29b39bf5fcc80ec5b79b19daeadf0e05d02cda0
Santosh Spring <santosh.spring32@gmail.com>
2017-01-26 22:10:05 +0530

    sf-dao-jdbctemplate-*: api elabrated
    Change-Id: I211eac6c935e0f3575ebb0cfaaf748554a27d995

---
+++ Notes_apps/template/sf-blog/sf-dao/6_JdbcTemplate/sf-dao-jdbctemplate-xml/src/main/resources/sql/insert-db.sql
@@ -0,0 +1,10 @@
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (1,'Garav 1',1001,'DB 1');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (3,'Garav 3',1003,'DB 3');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (2,'Garav 2',1002,'DB 2');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (4,'Garav 4',1004,'DB 4');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (5,'Garav 5',1005,'DB 5');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (6,'Garav 6',1006,'DB 6');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (7,'Garav 7',1007,'DB 7');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (8,'Garav 8',1008,'DB 8');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (9,'Garav 9',1009,'DB 9');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (10,'Garav 10',1010,'DB 10');
