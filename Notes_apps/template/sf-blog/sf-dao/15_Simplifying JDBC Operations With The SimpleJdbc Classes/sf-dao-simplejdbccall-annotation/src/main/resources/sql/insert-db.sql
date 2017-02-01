b667160c0b376ad0e7dc37bf19799ab8ef9dcd15
Spring32 <spring4buddies@gmail.com>
2017-02-01 13:42:14 +0530

    sf-dao-simplejdbccall-annotation: project structure added
    Change-Id: I31de5915a94fd0a60bd92ffefafa0a319acc8103

---
+++ Notes_apps/template/sf-blog/sf-dao/15_Simplifying JDBC Operations With The SimpleJdbc Classes/sf-dao-simplejdbccall-annotation/src/main/resources/sql/insert-db.sql
@@ -0,0 +1,24 @@
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (1,'Garav 1',1001,'DB 1');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (3,'Garav 3',1003,'DB 3');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (2,'Garav 2',1002,'DB 2');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (4,'Garav 4',1004,'DB 4');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (5,'Garav 5',1005,'DB 5');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (6,'Garav 6',1006,'DB 6');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (7,'Garav 7',1007,'DB 7');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (8,'Garav 8',1008,'DB 8');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (9,'Garav 9',1009,'DB 9');
+Insert into EMPLOYEE (EMP_ID,NAME,SALARY,DEPT) values (10,'Garav 10',1010,'DB 10');
+
+
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (1,'Cheryl',1001,'sales');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (3,'Kellin',1003,'hr');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (2,'Anthony',1002,'Account');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (4,'Austin ',1004,'front office');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (5,'Derrick',1005,'Account');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (6,'Franklin',1006,'hr');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (7,'Martin',1007,'DBA');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (8,'Douglas',1008,'back office');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (9,'Frederick',1009,'DBA');
+INSERT INTO customer (cust_id,NAME,salary,dept) VALUES (10,'David',1010,'sales');
+
+COMMIT;
