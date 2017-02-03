70f022195a5e17584ee9cc37ef46037c22439020
Santosh Spring <santosh.spring32@gmail.com>
2017-02-03 14:59:59 +0530

    sf-dao-simplejdbccall-* and sf-dao-embedded-database-support-*: are corrected
    Change-Id: Ifc5509e30470b240207e5bf9b19602ac7de4df63

---
+++ Notes_apps/template/sf-blog/sf-dao/18_Embedded Database Support/sf-dao-embedded-database-support-using-hsql-annotation/src/main/java/in/spring4buddies/application/Application.java
@@ -0,0 +1,33 @@
+package in.spring4buddies.application;
+
+import in.spring4buddies.application.dao.ProductDao;
+import in.spring4buddies.application.dao.ProductDaoImpl;
+import in.spring4buddies.application.model.Product;
+
+import java.util.List;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+	public static void main(String args[]) {
+		String confFile = "beans.xml";
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(confFile);
+
+		ProductDao productDao = (ProductDaoImpl) context.getBean("productDao");
+
+//		List<Product> products = productDao.getAllProducts();
+//		System.out.println("list of products - " + products);
+
+		int prod_id = RandomUtils.nextInt(10, 100);
+		Product product = new Product(prod_id, "VCD", 4500, "CD");
+
+		productDao.insertProduct(product);
+
+		List<Product> products = productDao.getProductById(prod_id);
+		System.out.println(products);
+
+		context.close();
+	}
+}
---
+++ Notes_apps/template/sf-blog/sf-dao/18_Embedded Database Support/sf-dao-embedded-database-support-using-hsql-annotation/src/main/resources/sql/insert-db.sql
@@ -0,0 +1,2 @@
+Insert into PRODUCTS (PROD_ID, NAME, PRICE, BRAND) values (101,'TE',1000,'TA');
+commit;
