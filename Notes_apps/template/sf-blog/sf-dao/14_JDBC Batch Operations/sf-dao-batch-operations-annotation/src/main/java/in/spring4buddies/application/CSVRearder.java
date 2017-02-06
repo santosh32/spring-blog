3aa726e73c583ad799acf13dab34e29a2a932d73
Spring32 <spring4buddies@gmail.com>
2017-02-06 19:52:19 +0530

    sf-dao-batch-operations-*: added classed and logic
    Change-Id: I3f6182979d2ae806ff44294686b5251b54724151

--- Notes_apps/template/sf-blog/sf-dao/14_JDBC Batch Operations/sf-dao-batch-operations-annotation/src/main/java/in/spring4buddies/application/CSVRearder.java
+++ Notes_apps/template/sf-blog/sf-dao/14_JDBC Batch Operations/sf-dao-batch-operations-annotation/src/main/java/in/spring4buddies/application/CSVRearder.java
@@ -2,8 +2,11 @@ package in.spring4buddies.application;
 
 import in.spring4buddies.application.model.Staff;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +14,12 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+
 public class CSVRearder {
-	
+
 	public static List<Staff> getStaffData() throws IOException {
 
 		InputStream inputStream = CSVRearder.class.getClassLoader().getResourceAsStream("staff.csv");
@@ -33,4 +40,32 @@ public class CSVRearder {
 		}
 		return staffs;
 	}
+
+	public static void main(String[] args) throws Exception {
+		getStaffData(null, null);
+	}
+
+	public static <T> void getStaffData(Class<T> clazz, String[] columns1) throws Exception {
+
+		ColumnPositionMappingStrategy<Staff> strategy = new ColumnPositionMappingStrategy<Staff>();
+		strategy.setType(Staff.class);
+
+		String[] columns = new String[] { "staffId", "lLame", "fName", "city", "state", "hPhone" };
+		strategy.setColumnMapping(columns);
+
+		CsvToBean<Staff> csvToBean = new CsvToBean<Staff>();
+
+		String csvFilename = "D:/poc/blogWS/code/spring-blog/Notes_apps/template/sf-blog/sf-dao/14_JDBC Batch Operations/sf-dao-batch-operations-annotation/src/main/resources/staff.csv";
+		CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
+
+		List<Staff> list = csvToBean.parse(strategy, csvReader);
+
+		for (Object object : list) {
+			Staff staff = (Staff) object;
+			System.out.println(staff.getStaffId() + " | " + staff.getlLame() + " | " + staff.getfName() + " | " + staff.getCity() + " | "
+					+ staff.getState() + " | " + staff.gethPhone());
+		}
+		System.out.println("Done");
+	}
+
 }
