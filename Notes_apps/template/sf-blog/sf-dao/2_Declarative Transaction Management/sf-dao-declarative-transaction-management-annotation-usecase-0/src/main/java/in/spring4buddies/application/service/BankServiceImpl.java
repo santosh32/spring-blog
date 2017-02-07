d770a64b4b5835024604469135164a8154acdff2
Santosh Spring <santosh.spring32@gmail.com>
2017-02-08 01:05:15 +0530

    sf-dao-declarative-transaction-management-annotation-usecase-0: created problem ...
    Change-Id: I926a182929d9eea0fd4c7c9aaa6ac0171a980500

--- Notes_apps/template/sf-blog/sf-dao/2_Declarative Transaction Management/sf-dao-declarative-transaction-management-annotation-usecase-0/src/main/java/in/spring4buddies/application/service/BankServiceImpl.java
+++ Notes_apps/template/sf-blog/sf-dao/2_Declarative Transaction Management/sf-dao-declarative-transaction-management-annotation-usecase-0/src/main/java/in/spring4buddies/application/service/BankServiceImpl.java
@@ -3,16 +3,12 @@ package in.spring4buddies.application.service;
 import in.spring4buddies.application.dao.BankDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional(propagation = Propagation.REQUIRED)
 public class BankServiceImpl implements BankService {
 
 	@Autowired
 	BankDao bankDao;
 
-	@Transactional(readOnly = true)
 	@Override
 	public double getBalance(int accountId) {
 		return bankDao.getBalance(accountId);
@@ -28,7 +24,6 @@ public class BankServiceImpl implements BankService {
 		bankDao.deposit(accountId, amount);
 	}
 
-	@Transactional(rollbackFor = Exception.class, noRollbackFor = Exception.class)
 	@Override
 	public void transfer(int primaryAccId, int beneficiaryAccId, double amount) throws Exception {
 		bankDao.transfer(primaryAccId, beneficiaryAccId, amount);
