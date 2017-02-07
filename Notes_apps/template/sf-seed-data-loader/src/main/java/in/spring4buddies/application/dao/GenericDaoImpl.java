997d98e9ab02cc59d90ad6b8bbaf8d5cf802fc67
Spring32 <spring4buddies@gmail.com>
2017-02-07 20:43:38 +0530

    adde generic dao
    Change-Id: I078ecf5332d8dc70b97808701abe6331fb2573cd

--- Notes_apps/template/sf-seed-data-loader/src/main/java/in/spring4buddies/application/dao/GenericDaoImpl.java
+++ Notes_apps/template/sf-seed-data-loader/src/main/java/in/spring4buddies/application/dao/GenericDaoImpl.java
@@ -1,30 +1,31 @@
 package in.spring4buddies.application.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class GenericDaoImpl<T> implements GenericDao<T> {
 
 	@Autowired
-	JdbcTemplate jdbcTemplate;
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
 	@Override
 	public void insert(List<T> data, Class<T> clazz) {
-		String query = "insert into employee (emp_id, name, salary, dept) values (?, ?, ?, ?)";
-		Object[] inputs = new Object[] {};
-		jdbcTemplate.update(query, inputs);
+		String query = "insert into employee (emp_id, name, salary, dept) values (:emp_id, :, :, :)";
+		namedParameterJdbcTemplate.update(query, new HashMap());
 	}
 
 	@Override
 	public List<T> getAll(Class<T> clazz) {
 		String query = "select * from " + clazz.getSimpleName();
-		return jdbcTemplate.query(query, new BeanPropertyRowMapper<T>(clazz));
+		return namedParameterJdbcTemplate.query(query, new BeanPropertyRowMapper<T>(clazz));
 	}
 
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
+	public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
 	}
+
 }
