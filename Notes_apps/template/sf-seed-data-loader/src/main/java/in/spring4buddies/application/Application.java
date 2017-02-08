e1f01df9f2a98ed02dfe01c5f55e9d5b05a0c81a
Spring32 <spring4buddies@gmail.com>
2017-02-08 21:12:50 +0530

    sf-seed-data-loader: reflection added Change-Id: Ic3a03568f65c93247532dcd1423eae511aaec714

--- Notes_apps/template/sf-seed-data-loader/src/main/java/in/spring4buddies/application/Application.java
+++ Notes_apps/template/sf-seed-data-loader/src/main/java/in/spring4buddies/application/Application.java
@@ -1,8 +1,12 @@
 package in.spring4buddies.application;
 
+import in.spring4buddies.application.csv.CSVHeader;
+import in.spring4buddies.application.csv.CSVRearder;
 import in.spring4buddies.application.dao.GenericDao;
 import in.spring4buddies.application.seeddata.Customer;
+import in.spring4buddies.application.seeddata.Employee;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import org.springframework.context.ConfigurableApplicationContext;
@@ -23,13 +27,28 @@ public class Application {
 		iterate(customers);
 
 		context.close();
+		
+		List<Employee> employees = CSVRearder.getDataByCSV(Employee.class);
+
+		for (Employee employee : employees) {
+			Field[] fields = employee.getClass().getDeclaredFields();
+			for (Field field : fields) {
+				field.setAccessible(true);
+				String name = field.getName();
+				System.out.println(name + "-" + field.get(employee));
+			}
+		}
 
 	}
 
+	// Insert into customer (customerId, contact, address, city, firstOrder,
+	// state, zipCode, country, phone, name ) values (14324742,'A. BAUM','5089
+	// CALERO AVENUE','SAN
+	// JOSE','05-Feb-70','CA','95123','USA','408629-0589','SANTA CLARA VALLEY
+	// TECHNOLOGY SPECIALISTS')
 	private static <T> void iterate(List<T> data) {
 		for (T t : data) {
 			System.out.println(t);
 		}
-
 	}
 }
