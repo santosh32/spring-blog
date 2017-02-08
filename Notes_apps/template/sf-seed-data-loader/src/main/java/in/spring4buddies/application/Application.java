4cc31c4193d04f9948669099ea0d42aab1bdfabb
Santosh Spring <santosh.spring32@gmail.com>
2017-02-09 00:15:22 +0530

    sf-seed-data-loader: modified
    Change-Id: I1aa4b1e8826406c13c72f48ba995dc3f5fd79384

--- Notes_apps/template/sf-seed-data-loader/src/main/java/in/spring4buddies/application/Application.java
+++ Notes_apps/template/sf-seed-data-loader/src/main/java/in/spring4buddies/application/Application.java
@@ -1,6 +1,6 @@
 package in.spring4buddies.application;
 
-import in.spring4buddies.application.csv.CSVRearder;
+import in.spring4buddies.application.dao.GenericDao;
 import in.spring4buddies.application.seeddata.Customer;
 import in.spring4buddies.application.seeddata.Employee;
 import in.spring4buddies.application.seeddata.Invoice;
@@ -9,38 +9,41 @@ import in.spring4buddies.application.seeddata.Staff;
 
 import java.util.List;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class Application {
 
 	public static void main(String args[]) throws Exception {
 
-//		String confFile = "beans.xml";
-//		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(confFile);
-//
-//		GenericDao genericDao = (GenericDao) context.getBean("genericDao");
-//
-//		List<Customer> customers = genericDao.getAll(Customer.class);
-//		iterate(customers);
-//
-//		List<Employee> employees = genericDao.getAll(Employee.class);
-//		iterate(employees);
-//
-//		List<Invoice> invoices = genericDao.getAll(Invoice.class);
-//		iterate(invoices);
-//
-//		List<OrderItem> orders = genericDao.getAll(OrderItem.class);
-//		iterate(orders);
-//
-//		List<Staff> staffs = genericDao.getAll(Staff.class);
-//		iterate(staffs);
-//
-//		context.close();
+		String confFile = "beans.xml";
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(confFile);
 
-		iterate(CSVRearder.getDataByCSV(Customer.class));
-		 iterate(CSVRearder.getDataByCSV(Employee.class));
-		 iterate(CSVRearder.getDataByCSV(Invoice.class));
-		 iterate(CSVRearder.getDataByCSV(OrderItem.class));
-		 iterate(CSVRearder.getDataByCSV(Staff.class));
+		GenericDao genericDao = (GenericDao) context.getBean("genericDao");
+
+		List<Customer> customers = genericDao.getAll(Customer.class);
+		iterate(customers);
+
+		List<Employee> employees = genericDao.getAll(Employee.class);
+		iterate(employees);
+
+		List<Invoice> invoices = genericDao.getAll(Invoice.class);
+		iterate(invoices);
+
+		List<OrderItem> orders = genericDao.getAll(OrderItem.class);
+		iterate(orders);
+
+		List<Staff> staffs = genericDao.getAll(Staff.class);
+		iterate(staffs);
+
+		context.close();
+
+		// iterate(CSVRearder.getDataByCSV(Customer.class));
+		// iterate(CSVRearder.getDataByCSV(Employee.class));
+		// iterate(CSVRearder.getDataByCSV(Invoice.class));
+		// iterate(CSVRearder.getDataByCSV(OrderItem.class));
+		// iterate(CSVRearder.getDataByCSV(Staff.class));
 
 		// iterate(DBStatement.insert(CSVRearder.getDataByCSV(Employee.class)));
 		// iterate(DBStatement.insert(CSVRearder.getDataByCSV(Invoice.class)));
