c49fb15efb7cc4a66e9afa2ff8b21feec6eab29b
Spring32 <spring4buddies@gmail.com>
2017-02-09 19:46:49 +0530

    sf-dao-transaction-management-propagation-requires: bl logic modified
    Change-Id: I5e5d603374cd9b79640cfaf491a130daf31a0524

---
+++ Notes_apps/template/sf-blog/sf-dao/1_Transaction Management/sf-dao-transaction-management-propagation-requires/src/main/java/in/spring4buddies/application/service/TravelService.java
@@ -0,0 +1,15 @@
+package in.spring4buddies.application.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TravelService {
+
+	Map<String, List<?>> getFlightsAndHotels(Date from, Date to);
+
+	void bookFlightsAndHotels(Date from, Date to);
+}
--- Notes_apps/template/sf-blog/sf-dao/1_Transaction Management/sf-dao-transaction-management-propagation-requires/src/main/java/in/spring4buddies/application/service/TravelServiceImpl.java
+++ Notes_apps/template/sf-blog/sf-dao/1_Transaction Management/sf-dao-transaction-management-propagation-requires/src/main/java/in/spring4buddies/application/service/TravelServiceImpl.java
@@ -4,21 +4,32 @@ import in.spring4buddies.application.model.Flight;
 import in.spring4buddies.application.model.Hotel;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TravelServiceImpl {
+public class TravelServiceImpl implements TravelService {
 
 	@Autowired
 	private HotelService hotelService;
 	@Autowired
 	private FlightService flightService;
 
-	public void getFlightsAndHotels(Date from, Date to) {
+	public Map<String, List<?>> getFlightsAndHotels(Date from, Date to) {
 		List<Flight> flights = flightService.getFlightsFor(from, to);
 		List<Hotel> hotels = hotelService.getHotelsFor(from, to);
+
+		Map<String, List<?>> map = new HashMap<String, List<?>>();
+		map.put(Flight.class.getSimpleName(), flights);
+		map.put(Hotel.class.getSimpleName(), hotels);
+		return map;
+	}
+
+	@Override
+	public void bookFlightsAndHotels(Date from, Date to) {
 	}
 }
