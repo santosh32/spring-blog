10a3d762dc0fcf24796e923f4296c36eb4471e31
Spring32 <spring4buddies@gmail.com>
2017-02-09 20:25:47 +0530

    sf-dao-transaction-management-propagation-requires: bl logic modified
    Change-Id: I73696c320f35c5560adf507eabe7721c2a548101

--- Notes_apps/template/sf-blog/sf-dao/1_Transaction Management/sf-dao-transaction-management-propagation-requires/src/main/resources/sql/insert-db.sql
+++ Notes_apps/template/sf-blog/sf-dao/1_Transaction Management/sf-dao-transaction-management-propagation-requires/src/main/resources/sql/insert-db.sql
@@ -1,3 +1,3 @@
 INSERT INTO Flight (flight_Id, name, type, booked_from, booked_to) VALUES (1,'King Fisher', 'business', '2017-02-09', '2017-02-09')
 
-INSERT INTO Hotel (Hotel_Id, name, type, booked_from, booked_to) VALUES (1,'King Fisher1', 'business', '2017-02-09', '2017-02-09')
+INSERT INTO Hotel (Hotel_Id, name, type, booked_from, booked_to) VALUES (1,'Radieson', 'business', '2017-02-09', '2017-02-09')
