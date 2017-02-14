301af61bc48435fb3522ca5908dfd63e8b6dbff4
Spring32 <spring4buddies@gmail.com>
2017-02-14 11:29:11 +0530

    sf-dao-simplejdbccall-stored-function-annotation: added bl logic
    Change-Id: If0da5b9437445ab9dc5f0b7df008d91af4d9e172

--- Notes_apps/template/sf-blog/sf-dao/15_Simplifying JDBC Operations With The SimpleJdbc Classes/sf-dao-simplejdbccall-stored-function-annotation/src/main/java/in/spring4buddies/application/dao/ProductDao.java
+++ Notes_apps/template/sf-blog/sf-dao/15_Simplifying JDBC Operations With The SimpleJdbc Classes/sf-dao-simplejdbccall-stored-function-annotation/src/main/java/in/spring4buddies/application/dao/ProductDao.java
@@ -1,14 +1,7 @@
 package in.spring4buddies.application.dao;
 
-import in.spring4buddies.application.model.Product;
-
-import java.util.List;
-
 public interface ProductDao {
 
-	List<Product> getAllProducts();
+	Double getPriceById(int prod_id);
 
-	List<Product> getProductById(int prod_id);
-
-	void insertProduct(Product product);
 }
