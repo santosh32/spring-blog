04ed17d0ea4b8c1accd941a7ee998f1f561d3861
Santosh Spring <santosh.spring32@gmail.com>
2017-02-14 07:00:55 +0530

    sf-dao-simplejdbcinsert-retrieving-auto-generated-keys: project added
    Change-Id: Ib26aed10c72a8d4099575df2d70be1b79563f0bc

---
+++ Notes_apps/template/sf-blog/sf-dao/15_Simplifying JDBC Operations With The SimpleJdbc Classes/sf-dao-simplejdbcinsert-retrieving-auto-generated-keys/src/main/java/in/spring4buddies/application/Application.java
@@ -0,0 +1,26 @@
+package in.spring4buddies.application;
+
+import in.spring4buddies.application.dao.MovieDao;
+import in.spring4buddies.application.model.Movie;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+	public static void main(String args[]) {
+		String confFile = "beans.xml";
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(confFile);
+
+		MovieDao movieDao = (MovieDao) context.getBean("movieDao");
+
+		Movie movie = new Movie();
+		movie.setTitle(" Captain America: Civil War");
+		movie.setDirector("Anthony Russo, Joe Russo");
+		movie.setGenre("Action & Adventure, Science Fiction & Fantasy");
+		movie.setRaiting("4.1");
+		movie.setBoxOffice("$408,080,554.00");
+		movieDao.addMovie(movie);
+		
+		context.close();
+	}
+}
