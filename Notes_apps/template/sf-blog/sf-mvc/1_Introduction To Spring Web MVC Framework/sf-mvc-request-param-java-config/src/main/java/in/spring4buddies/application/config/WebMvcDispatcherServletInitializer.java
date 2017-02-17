a3f60a1f089993b858b16446efc82b743e515dba
Santosh Spring <santosh.spring32@gmail.com>
2017-02-17 12:29:26 +0530

    sf-mvc-inputparam-*:project structure added
    Change-Id: Ic3bf7c1c3208ccfd404d8198e20626648a3de526

---
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-param-java-config/src/main/java/in/spring4buddies/application/config/WebMvcDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+/*package in.spring4buddies.application.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { ApplicationContextConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}*/
