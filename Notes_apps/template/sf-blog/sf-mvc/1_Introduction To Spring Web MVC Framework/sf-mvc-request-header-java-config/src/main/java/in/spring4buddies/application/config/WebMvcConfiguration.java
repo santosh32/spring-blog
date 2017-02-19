5110fabacf1d1e9b5b7d93a667e9a8d1804cd2d4
Santosh Spring <santosh.spring32@gmail.com>
2017-02-19 22:14:51 +0530

    sf-mvc-request-header-java-config: project structure added
    Change-Id: I378bbf1e9c8eada8170b056dbf73f01420a055c5

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-header-java-config/src/main/java/in/spring4buddies/application/config/WebMvcConfiguration.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-header-java-config/src/main/java/in/spring4buddies/application/config/WebMvcConfiguration.java
@@ -3,10 +3,8 @@ package in.spring4buddies.application.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 @EnableWebMvc
@@ -24,14 +22,4 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
-
-	//<mvc:annotation-driven enable-matrix-variables="true"/>
-	@Override
-	public void configurePathMatch(PathMatchConfigurer configurer) {
-		UrlPathHelper urlPathHelper = new UrlPathHelper();
-		urlPathHelper.setRemoveSemicolonContent(false);
-		configurer.setUrlPathHelper(urlPathHelper);
-	}
-	
-	
 }
