7a79fbbb5ec667443a7a5d80a2eb8ffe4602b50d
Santosh Spring <santosh.spring32@gmail.com>
2017-02-20 23:09:06 +0530

    sf-mvc-xml-response-java-config: bl added
    Change-Id: Ib05fe4598063394a036163bad8cf5eba765509fc

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-xml-response-java-config/src/main/java/in/spring4buddies/application/config/WebMvcInitializer.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-xml-response-java-config/src/main/java/in/spring4buddies/application/config/WebMvcInitializer.java
@@ -13,7 +13,7 @@ public class WebMvcInitializer implements WebApplicationInitializer {
 	public void onStartup(ServletContext container) throws ServletException {
 
 		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-		ctx.register(ApplicationContextConfiguration.class);
+		ctx.register(WebMvcConfiguration.class);
 		ctx.setServletContext(container);
 
 		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
