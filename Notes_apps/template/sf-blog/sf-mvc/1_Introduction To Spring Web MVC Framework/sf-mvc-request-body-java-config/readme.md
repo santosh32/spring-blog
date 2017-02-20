fc98e610c0ea037896160e4e7347821932c5eb84
Santosh Spring <santosh.spring32@gmail.com>
2017-02-20 23:44:11 +0530

    sf-mvc-request-body-java-config: bl added
    Change-Id: Ib660b17a19ad20f5840be5be22e6017ed1003ea5

---
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-body-java-config/readme.md
@@ -0,0 +1,4 @@
+mvn clean install
+mvn tomcat7:run
+
+http://localhost:8080/sf-mvc-request-body-java-config/employee
--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-body-java-config/src/main/java/in/spring4buddies/application/controller/EmployeeController.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-body-java-config/src/main/java/in/spring4buddies/application/controller/EmployeeController.java
@@ -3,30 +3,26 @@ package in.spring4buddies.application.controller;
 import in.spring4buddies.application.model.Employee;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EmployeeController {
 
 	@RequestMapping(value = "/employee", method = RequestMethod.GET)
-	public ModelAndView newEmployee() {
-		ModelAndView mv = new ModelAndView("employee");
-		mv.addObject("command", new Employee());
-		return mv;
+	public String newEmployee() {
+		return "employee";
 	}
 
 	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
-	public ModelAndView addEmployee(@ModelAttribute("employee") Employee employee) {
-
-		// Save the Employee data in Database
+	public @ResponseBody ModelAndView addEmployee(@RequestBody Employee employee) {
 
 		ModelAndView mv = new ModelAndView("success");
 		mv.addObject("name", employee.getFirstName() + " " + employee.getLastName());
 
 		return mv;
-
 	}
 }
