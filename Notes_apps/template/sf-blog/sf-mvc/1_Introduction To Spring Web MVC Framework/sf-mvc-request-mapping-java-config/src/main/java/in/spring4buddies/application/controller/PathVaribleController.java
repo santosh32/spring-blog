63da7f0e3031b1ff066b787425ccc198655f3fef
Spring32 <spring4buddies@gmail.com>
2017-02-22 11:01:35 +0530

    sf-mvc-request-mapping-java-config: template structure added
    Change-Id: I47fe4141d3a014a40381e30642236e3637e7df57

---
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-mapping-java-config/src/main/java/in/spring4buddies/application/controller/PathVaribleController.java
@@ -0,0 +1,35 @@
+package in.spring4buddies.application.controller;
+
+import in.spring4buddies.application.command.Employee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PathVaribleController {
+
+	@RequestMapping(value = "/{name}/{address}/{city}/{state}/{country}", method = RequestMethod.GET)
+	public String showEmployeeDetails(ModelMap model, 
+			@PathVariable(value = "name") String name, 
+			@PathVariable(value = "address") String address,
+			@PathVariable(value = "city") String city, 
+			@PathVariable(value = "state") String state, 
+			@PathVariable(value = "country") String country) {
+
+		Employee employee = new Employee(name, address, city, state, country);
+		model.addAttribute("employee", employee);
+		return "employee";
+	}
+	
+	@RequestMapping(value = "/employee/{name}/{email:.+}", method = RequestMethod.GET)
+	public String addEmployee(ModelMap model,
+			@PathVariable(value = "name") String name, 
+			@PathVariable(value = "email") String email) {
+
+		return "employee";
+	}
+
+}
