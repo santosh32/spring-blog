27aac86906f564c3d649b9b4ac1d5056d408ae12
Santosh Spring <santosh.spring32@gmail.com>
2017-02-24 23:30:50 +0530

    sf-mvc-request-param-java-config: bl added 3
    Change-Id: I340f60d487ec54fcaa4bf143363c0193d6882a5e

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-param-java-config/readme.md
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-param-java-config/readme.md
@@ -4,3 +4,5 @@ mvn tomcat7:run
 http://localhost:8080/sf-mvc-request-param-java-config/employee?name=Greg&address=HitechCity&city=Hyd&state=TS&country=IN
 
 http://localhost:8080/sf-mvc-request-param-java-config/RequestParam?name=Greg&address=HitechCity&city=Hyd&state=TS&country=IN
+http://localhost:8080/sf-mvc-request-param-java-config/RequestParam/map?name=Greg&address=HitechCity&city=Hyd&state=TS&country=IN
+http://localhost:8080/sf-mvc-request-param-java-config/RequestParam/dateformat?dateOfBirth=1980-10-31&hireDate=2017-03-03
--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-param-java-config/src/main/java/in/spring4buddies/application/controller/RequestParamController.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-request-param-java-config/src/main/java/in/spring4buddies/application/controller/RequestParamController.java
@@ -1,5 +1,9 @@
 package in.spring4buddies.application.controller;
 
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +15,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class RequestParamController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String showEmployeeDetails(ModelMap model, @RequestParam(value = "name") String name,
+	public String showEmployeeDetails(@RequestParam(value = "name") String name, 
 			@RequestParam(value = "address") String line,
-			@RequestParam String city, 
+			@RequestParam String city,
 			@RequestParam(value = "state") String state, 
-			@RequestParam(value = "country") String country) {
+			@RequestParam(value = "country") String country, ModelMap model) {
 
 		String message = String.format("%s, %s, %s, %s, %s", name, line, city, state, country);
 		model.addAttribute("message", message);
-		
+
 		return "displayRequestParam";
 	}
 
+	@RequestMapping(value = "/map", method = RequestMethod.GET)
+	public String showEmployeeByMap(@RequestParam Map<String, String> varsMap, ModelMap model) {
+		
+		model.addAttribute("message", varsMap.toString());
+		
+		return "displayRequestParam";
+	}
+	
+	@RequestMapping(value = "/dateformat", method = RequestMethod.GET)
+	public String showEmployeeByHireAndDOb(
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam(value = "dateOfBirth") Date dateOfBirth,
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam(value = "hireDate") Date hireDate, ModelMap model) {
+
+		String message = String.format("DateOfBirth: %s<br> HireDate: %s", dateOfBirth, hireDate);
+		model.addAttribute("message", message);
+
+		return "displayRequestParam";
+	}
 }
