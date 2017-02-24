c97f1c219d542389dcb35986c38d05a49d8a3290
Santosh Spring <santosh.spring32@gmail.com>
2017-02-24 22:42:52 +0530

    Change-Id: I1605683ff49f5507962570b2209e9b0b5a0ec3d3

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/readme.md
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/readme.md
@@ -4,3 +4,11 @@ mvn tomcat7:run
 http://localhost:8080/sf-mvc-path-variable-java-config/employee/Greg/HitechCity/Hyd/TS/India
 
 http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/Greg/HitechCity/Hyd/TS/India
+http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/Greg/HitechCity
+
+#Ambiguous error
+http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/Greg
+http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/123
+
+http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/Greg
+http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/123
--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/src/main/java/in/spring4buddies/application/controller/PathVariableController.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/src/main/java/in/spring4buddies/application/controller/PathVariableController.java
@@ -1,5 +1,7 @@
 package in.spring4buddies.application.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,25 +13,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PathVariableController {
 
 	@RequestMapping(value = "/{name}/{address}/{city}/{state}/{country}", method = RequestMethod.GET)
-	public String showEmployeeDetails(ModelMap model, 
+	public String showEmployeeByPathVariable(ModelMap model, 
 			@PathVariable(value = "name") String name, 
-			@PathVariable(value = "address") String address,
+			@PathVariable(value = "address") String line,
 			@PathVariable String city, 
 			@PathVariable String state, 
 			@PathVariable(value = "country") String country) {
 
-		String message = String.format("%s, %s, %s, %s, %s", name, address, city, state, country);
+		String message = String.format("%s, %s, %s, %s, %s", name, line, city, state, country);
 		model.addAttribute("message", message);
 		
 		return "displayPathVariable";
 	}
 	
-	@RequestMapping(value = "/employee/{name}/{email:.+}", method = RequestMethod.GET)
-	public String addEmployee(ModelMap model,
-			@PathVariable(value = "name") String name, 
-			@PathVariable(value = "email") String email) {
+	@RequestMapping(value = "/{name}/{address}", method = RequestMethod.GET)
+	public String showEmployeeByMap(@PathVariable Map<String, String> varsMap, ModelMap model) {
+		model.addAttribute("message", varsMap.toString());
+		return "displayPathVariable";
+	}
+	
+//	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+//	public String showEmployeeName(@PathVariable String name, ModelMap model) {
+//		model.addAttribute("message", name);
+//		return "displayPathVariable";
+//	}
+//
+//	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//	public String showEmployeeId(@PathVariable String id, ModelMap model) {
+//		model.addAttribute("message", id);
+//		return "displayPathVariable";
+//	}
+	
+	@RequestMapping(value = "{name:[a-zA-Z]+}", method = RequestMethod.GET)
+	public String showEmployeeName(@PathVariable String name, ModelMap model) {
+		model.addAttribute("message", "This is from showEmployeeName() - " + name);
+		return "displayPathVariable";
+	}
 
-		return "employee";
+	@RequestMapping(value = "{id:[0-9]+}", method = RequestMethod.GET)
+	public String showEmployeeId(@PathVariable String id, ModelMap model) {
+		model.addAttribute("message", "This is from showEmployeeId() - " + id);
+		return "displayPathVariable";
 	}
 
 }
