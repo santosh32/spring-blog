23c272b31a949e58aaae878e4ffcfa232f0185d2
Santosh Spring <santosh.spring32@gmail.com>
2017-02-24 22:22:21 +0530

    Change-Id: Ia88a2e943c8a5772867235578f05ae0b9e9a75df

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/readme.md
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/readme.md
@@ -1,4 +1,6 @@
 mvn clean install
 mvn tomcat7:run
 
-http://localhost:8080/sf-mvc-path-variable-java-config/Greg/HitechCity/hyd/ts/india
+http://localhost:8080/sf-mvc-path-variable-java-config/employee/Greg/HitechCity/Hyd/TS/India
+
+http://localhost:8080/sf-mvc-path-variable-java-config/PathVariable/Greg/HitechCity/Hyd/TS/India
--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/src/main/java/in/spring4buddies/application/controller/PathVariableController.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/sf-mvc-path-variable-java-config/src/main/java/in/spring4buddies/application/controller/PathVariableController.java
@@ -1,7 +1,5 @@
 package in.spring4buddies.application.controller;
 
-import in.spring4buddies.application.command.Employee;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,19 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class PathVaribleController {
+@RequestMapping("/PathVariable")
+public class PathVariableController {
 
 	@RequestMapping(value = "/{name}/{address}/{city}/{state}/{country}", method = RequestMethod.GET)
 	public String showEmployeeDetails(ModelMap model, 
 			@PathVariable(value = "name") String name, 
 			@PathVariable(value = "address") String address,
-			@PathVariable(value = "city") String city, 
-			@PathVariable(value = "state") String state, 
+			@PathVariable String city, 
+			@PathVariable String state, 
 			@PathVariable(value = "country") String country) {
 
-		Employee employee = new Employee(100l, name, address, city, state, country);
-		model.addAttribute("employee", employee);
-		return "employee";
+		String message = String.format("%s, %s, %s, %s, %s", name, address, city, state, country);
+		model.addAttribute("message", message);
+		
+		return "displayPathVariable";
 	}
 	
 	@RequestMapping(value = "/employee/{name}/{email:.+}", method = RequestMethod.GET)
