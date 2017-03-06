eec8cdb597042ce9fd46876dd3927303c987dd31
Spring32 <spring4buddies@gmail.com>
2017-03-06 14:11:24 +0530

    sf-oxm-jaxb-java-config: bl added
    Change-Id: I33fe1ef2ea5116ff48540c00ace633716d010f87

--- Notes_apps/template/sf-blog/sf-oxm/1_Introduction/sf-oxm-jaxb-java-config/src/main/java/in/spring4buddies/application/Application.java
+++ Notes_apps/template/sf-blog/sf-oxm/1_Introduction/sf-oxm-jaxb-java-config/src/main/java/in/spring4buddies/application/Application.java
@@ -1,8 +1,34 @@
 package in.spring4buddies.application;
 
-public class Application { // implements CommandLineRunner {
+import in.spring4buddies.application.jaxb.Address;
+import in.spring4buddies.application.jaxb.Student;
+import in.spring4buddies.application.jaxb.Students;
+import in.spring4buddies.application.jaxb.config.JaxbConfig;
+import in.spring4buddies.application.jaxb.helper.JaxbHelper;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
 
 	public static void main(String[] args) throws Exception {
 
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JaxbConfig.class);
+
+		JaxbHelper jaxbHelper = context.getBean(JaxbHelper.class);
+
+		Address address = new Address("TS", "HYD", "Madhapur", "510103");
+		Student student = new Student(1, "suresh", "suresh.b@gmail.com", address);
+
+		Address address1 = new Address("TS", "HYD", "Hitech city", "510102");
+		Student student1 = new Student(1, "mahesh", "suresh.k@gmail.com", address1);
+
+		Students students = new Students();
+		students.addStudent(student);
+		students.addStudent(student1);
+		jaxbHelper.marshal(students, "student-out.xml");
+
+		System.out.println(jaxbHelper.unmarshal("student.xml"));
+
+		context.close();
 	}
 }
---
+++ Notes_apps/template/sf-blog/sf-oxm/1_Introduction/sf-oxm-jaxb-java-config/src/main/java/in/spring4buddies/application/jaxb/config/JaxbConfig.java
@@ -0,0 +1,35 @@
+package in.spring4buddies.application.jaxb.config;
+
+import in.spring4buddies.application.jaxb.helper.JaxbHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class JaxbConfig {
+
+	@Bean
+	public JaxbHelper JaxbHelper(Jaxb2Marshaller jaxb2Marshaller) {
+		JaxbHelper jaxbHelper = new JaxbHelper();
+		jaxbHelper.setMarshaller(jaxb2Marshaller);
+		jaxbHelper.setUnmarshaller(jaxb2Marshaller);
+		return jaxbHelper;
+	}
+
+	@Bean
+	public Jaxb2Marshaller getJaxbMarshaller() {
+		Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+		jaxb2Marshaller.setPackagesToScan("in.spring4buddies.application.jaxb");
+
+		Map<String, Object> props = new HashMap<String, Object>();
+		props.put(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+		jaxb2Marshaller.setMarshallerProperties(props);
+
+		return jaxb2Marshaller;
+	}
+}
