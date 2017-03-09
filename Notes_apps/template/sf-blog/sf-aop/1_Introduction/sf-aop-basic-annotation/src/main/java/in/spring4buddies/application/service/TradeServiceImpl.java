d7419cfe0be00988760cb49ad91414feddf66fe3
Santosh Spring <santosh.spring32@gmail.com>
2017-03-09 07:59:39 +0530

    >sf-aop-basic-annotation: added template
    Change-Id: I5ce3aa812d0f14d03f58124cbb8db8f5f0b1213b

---
+++ Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-basic-annotation/src/main/java/in/spring4buddies/application/service/TradeServiceImpl.java
@@ -0,0 +1,33 @@
+package in.spring4buddies.application.service;
+
+import org.springframework.stereotype.Service;
+
+import in.spring4buddies.application.model.Trade;
+
+@Service
+public class TradeServiceImpl implements TradeService {
+
+	@Override
+	public Trade getTradeByType(String tradeType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTrade(Trade trade) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Trade updateTrade(Trade trade, String tradeType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteTrade(String tradeType) {
+		// TODO Auto-generated method stub
+
+	}
+}
