bc62dcce58680e79a92c55bd73d06dd4847ad1d2
Santosh Spring <santosh.spring32@gmail.com>
2017-03-13 15:12:43 +0530

    sf-aop-basic-annotation: bl modified Change-Id: Ia0adef32253b9624fdf5219084dcd548ae48482d

--- Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-basic-annotation/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
+++ Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-basic-annotation/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
@@ -1,6 +1,7 @@
 package in.spring4buddies.application.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 public class LoggingAspect {
 
@@ -10,5 +11,20 @@ public class LoggingAspect {
 
 	public void logAfter(JoinPoint joinPoint) {
 		System.out.println("LoggingAspect.logAfter() : " + joinPoint.getSignature().getName());
+	}
+
+	public void logAfterReturning(JoinPoint joinPoint, Object retVal) {
+		System.out.println("LoggingAspect.logAfterReturning() " + joinPoint.getSignature().getName() + " : retVal :" + retVal);
+	}
+
+	public void logAfterThrowing(JoinPoint joinPoint, Exception ex) {
+		System.out.println("LoggingAspect.logAfterThrowing() " + joinPoint.getSignature().getName() + " : Exception :" + ex);
+	}
+
+	public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("LoggingAspect.logAround() : before : " + pjp.getSignature().getName());
+		Object object = pjp.proceed();
+		System.out.println("LoggingAspect.logAround() : after : " + pjp.getSignature().getName());
+		return object;
 	}
 }
