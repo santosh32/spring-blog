462b594655c1b03bf5f4c609436e7c257815c9a3
Santosh Spring <santosh.spring32@gmail.com>
2017-03-13 21:22:48 +0530

    sf-aop-pointcut-designators: args pcd added
    Change-Id: I7465fba5a0b9ecee48d4a5acfdbf9cd8cc0f6ed9

--- Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-pointcut-designators/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
+++ Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-pointcut-designators/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
@@ -1,6 +1,7 @@
 package in.spring4buddies.application.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -22,7 +23,7 @@ public class LoggingAspect {
 		System.out.println("PCD - execution() LoggingAspect.logBeforeDaoGetMethod() : daoGetMethod() : " + joinPoint.getSignature().getName());
 	}
 
-	// within pcd - this is package level
+	// within pcd - this is package level only
 	@Pointcut("within(in.spring4buddies.application.dao.TradeDaoImpl)")
 	public void daoMethod() {
 
@@ -31,5 +32,16 @@ public class LoggingAspect {
 	@Before("daoMethod()")
 	public void logBeforeDaoAddMethod(JoinPoint joinPoint) {
 		System.out.println("PCD - within() LoggingAspect.logBeforeDaoAddMethod() : daoMethod() : " + joinPoint.getSignature().getName());
+	}
+
+	// args pcd
+	@Pointcut("execution(* in.spring4buddies.application.service.*.get*(String))")
+	public void serviceGetMethod() {
+
+	}
+
+	@After("serviceGetMethod()")
+	public void logAfterServiceGetMethod(JoinPoint joinPoint) {
+		System.out.println("PCD - args() LoggingAspect.logAfterServiceGetMethod() : serviceGetMethod() : " + joinPoint.getSignature().getName());
 	}
 }
