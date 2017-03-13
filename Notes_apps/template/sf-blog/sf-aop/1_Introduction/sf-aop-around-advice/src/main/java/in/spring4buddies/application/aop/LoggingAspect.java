29e2c173f721a766f3a048c79c7f9339cc86b316
Santosh Spring <santosh.spring32@gmail.com>
2017-03-13 11:25:20 +0530

    sf-aop-around-advice: ProceedingJoinPoint added to track around advice changes
    Change-Id: Iee399114932d7fbb9bb297885ab96a4239d3b874

--- Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-around-advice/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
+++ Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-around-advice/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
@@ -1,6 +1,6 @@
 package in.spring4buddies.application.aop;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
 	@Around("execution(* in.spring4buddies.application.service.*.*(..))")
-	public void logAround(JoinPoint joinPoint) {
-		System.out.println("LoggingAspect.logAround() : " + joinPoint.getSignature().getName());
+	public void logAround(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("LoggingAspect.logAround() : before : " + pjp.getSignature().getName());
+		pjp.proceed();
+		System.out.println("LoggingAspect.logAround() : after : " + pjp.getSignature().getName());
 	}
 }
