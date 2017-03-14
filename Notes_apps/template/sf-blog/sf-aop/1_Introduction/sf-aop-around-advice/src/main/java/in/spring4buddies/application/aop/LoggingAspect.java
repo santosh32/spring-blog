dc498391dacea2dd3df59463be793690ccd027da
Santosh Spring <santosh.spring32@gmail.com>
2017-03-14 10:30:13 +0530

    sf-aop-around-advice: around advice logic enhanced
    Change-Id: I0adbe754b3e9742b4766b3adb5f90380c19329d1

--- Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-around-advice/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
+++ Notes_apps/template/sf-blog/sf-aop/1_Introduction/sf-aop-around-advice/src/main/java/in/spring4buddies/application/aop/LoggingAspect.java
@@ -11,8 +11,22 @@ public class LoggingAspect {
 
 	@Around("execution(* in.spring4buddies.application.service.*.*(..))")
 	public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
+		Object object = null;
+
 		System.out.println("LoggingAspect.logAround() : before : " + pjp.getSignature().getName());
-		Object object = pjp.proceed();
+		long t0 = System.currentTimeMillis();
+
+		try {
+			object = pjp.proceed();
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
+		}
+
+		long t1 = System.currentTimeMillis();
+		String methodName = pjp.getSignature().getName();
+
+		System.out.println(Thread.currentThread().getName() + " method: " + methodName + " took " + (t1 - t0) + " milliseconds");
+
 		System.out.println("LoggingAspect.logAround() : after : " + pjp.getSignature().getName());
 		return object;
 	}
