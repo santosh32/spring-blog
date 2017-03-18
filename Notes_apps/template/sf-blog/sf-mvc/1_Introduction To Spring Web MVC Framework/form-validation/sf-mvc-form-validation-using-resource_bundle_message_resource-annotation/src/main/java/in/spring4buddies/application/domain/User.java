98bc3d2e0ba94d680cf35839749378faac1c5523
Santosh Spring <santosh.spring32@gmail.com>
2017-03-18 14:57:21 +0530

    Change-Id: I3e8fb58f2d6a2e1aedbe1fcc85241612fcdc3e7e

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/form-validation/sf-mvc-form-validation-using-resource_bundle_message_resource-annotation/src/main/java/in/spring4buddies/application/domain/User.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/form-validation/sf-mvc-form-validation-using-resource_bundle_message_resource-annotation/src/main/java/in/spring4buddies/application/domain/User.java
@@ -1,32 +1,13 @@
 package in.spring4buddies.application.domain;
 
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotEmpty;
-
 public class User {
 
-	@NotEmpty(message = "UserName cannot be empty")
-	@Size(min = 1, max = 16, message = "Size should be between 1 to 16")
 	private String name;
-
-	@NotEmpty(message = "Password cannot be empty")
-	@Size(min = 1, max = 16, message = "Size should be between 1 to 16")
 	private String password;
-
-	@NotEmpty(message = "Gender cannot be empty")
 	private String gender;
-
-	@NotEmpty(message = "Country cannot be empty")
 	private String country;
-
-	@NotEmpty(message = "AboutYou cannot be empty")
-	@Size(min = 1, max = 50, message = "Size should be between 1 to 50")
 	private String aboutYou;
-
-	@NotEmpty(message = "Community cannot be empty")
 	private String[] community;
-
 	private Boolean feed;
 
 	public String getName() {
