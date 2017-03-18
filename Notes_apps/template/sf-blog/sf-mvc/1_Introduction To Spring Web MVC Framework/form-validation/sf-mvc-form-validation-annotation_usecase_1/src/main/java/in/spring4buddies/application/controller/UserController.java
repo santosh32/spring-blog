8eaae1f768dfbfa5eb75b1edcb69a5c315d9f3b0
Santosh Spring <santosh.spring32@gmail.com>
2017-03-18 15:24:46 +0530

    sf-mvc-form-validation-annotation_usecase_1:bl added Change-Id: I8324270ca90f675b6abcccd043c5a2709ad3c5bf

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/form-validation/sf-mvc-form-validation-annotation_usecase_1/src/main/java/in/spring4buddies/application/controller/UserController.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/form-validation/sf-mvc-form-validation-annotation_usecase_1/src/main/java/in/spring4buddies/application/controller/UserController.java
@@ -1,5 +1,12 @@
 package in.spring4buddies.application.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
@@ -10,6 +17,35 @@ public class UserController extends SimpleFormController {
 	public UserController() {
 		setCommandClass(User.class);
 		setCommandName("userForm");
+	}
+
+	@Override
+	protected Object formBackingObject(HttpServletRequest request) throws Exception {
+		User user = new User();
+		user.setCountry("India");
+		return user;
+	}
+
+	protected Map<String, Object> referenceData(HttpServletRequest request) throws Exception {
+
+		Map<String, Object> referenceData = new HashMap<>();
+
+		List<String> communities = new ArrayList<>();
+		communities.add("Spring");
+		communities.add("Hibernate");
+		communities.add("Struts");
+
+		List<String> countries = new ArrayList<>();
+		countries.add("Select");
+		countries.add("India");
+		countries.add("USA");
+		countries.add("UK");
+
+		referenceData.put("communities", communities);
+		referenceData.put("countries", countries);
+
+		return referenceData;
+
 	}
 
 	@Override
