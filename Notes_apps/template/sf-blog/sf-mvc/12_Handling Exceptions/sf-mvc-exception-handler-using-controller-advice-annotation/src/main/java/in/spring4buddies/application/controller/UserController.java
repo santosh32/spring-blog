fe729c22d2c0a6790b8764fb785cd50a52df1d89
Santosh Spring <santosh.spring32@gmail.com>
2017-03-18 16:20:00 +0530

    Change-Id: Ibcc2bea36fb36abac5aebdbe0c8325d8a10b64a5

--- Notes_apps/template/sf-blog/sf-mvc/12_Handling Exceptions/sf-mvc-exception-handler-using-controller-advice-annotation/src/main/java/in/spring4buddies/application/controller/UserController.java
+++ Notes_apps/template/sf-blog/sf-mvc/12_Handling Exceptions/sf-mvc-exception-handler-using-controller-advice-annotation/src/main/java/in/spring4buddies/application/controller/UserController.java
@@ -9,11 +9,9 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import in.spring4buddies.application.domain.User;
 
