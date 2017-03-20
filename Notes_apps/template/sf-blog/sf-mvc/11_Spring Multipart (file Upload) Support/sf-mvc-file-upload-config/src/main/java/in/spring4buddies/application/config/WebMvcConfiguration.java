1fbc446bc6273fa3f54da97aa461850513f87fa1
Santosh Spring <santosh.spring32@gmail.com>
2017-03-20 07:54:36 +0530

    sf-mvc-file-upload-config
    Change-Id: I166b83413028f4945cfaa4dbdd3d7d77eade2aac

--- Notes_apps/template/sf-blog/sf-mvc/11_Spring Multipart (file Upload) Support/sf-mvc-file-upload-config/src/main/java/in/spring4buddies/application/config/WebMvcConfiguration.java
+++ Notes_apps/template/sf-blog/sf-mvc/11_Spring Multipart (file Upload) Support/sf-mvc-file-upload-config/src/main/java/in/spring4buddies/application/config/WebMvcConfiguration.java
@@ -1,8 +1,11 @@
 package in.spring4buddies.application.config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -32,6 +35,14 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 		resolver.setViewClass(JstlView.class);
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		// no limit
+		resolver.setMaxUploadSize(-1);
 		return resolver;
 	}
 }
