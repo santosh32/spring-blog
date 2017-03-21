4861b938e00f6c45548be92619e7779a06dae54b
Santosh Spring <santosh.spring32@gmail.com>
2017-03-21 09:43:47 +0530

    sf-mvc-init-binder-annotation
    Change-Id: Ibeb068811f3f20e7a31226f189e31b0d9224b2c7

---
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/init-binder/sf-mvc-init-binder-annotation/readme.md
@@ -0,0 +1,6 @@
+mvn clean install
+
+mvn tomcat7:run
+
+http://localhost:8080/sf-mvc-init-binder-annotation/
+http://localhost:8080/sf-mvc-init-binder-annotation/user
---
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/init-binder/sf-mvc-init-binder-annotation/src/main/java/in/spring4buddies/application/controller/UserController.java
@@ -0,0 +1,31 @@
+package in.spring4buddies.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import in.spring4buddies.application.command.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView get() {
+
+		ModelAndView modelAndView = new ModelAndView("userForm");
+		// Step 1.
+		// modelAndView.addObject("command", new User());
+		// Step 2 & 3.
+		modelAndView.addObject("user", new User());
+		return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String post(User user, BindingResult bindingResult, Model model) {
+		return "userSuccess";
+	}
+}
