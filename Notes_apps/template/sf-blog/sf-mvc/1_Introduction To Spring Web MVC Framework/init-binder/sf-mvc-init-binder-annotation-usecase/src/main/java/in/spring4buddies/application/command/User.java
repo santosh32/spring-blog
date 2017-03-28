2d6e252732fcda9b95a2a443bdd8f2bdd5be0414
Santosh Spring <santosh.spring32@gmail.com>
2017-03-28 19:18:49 +0530

    Change-Id: I3fc683100e22205099a82b19df5ea7a31fab3827

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/init-binder/sf-mvc-init-binder-annotation-usecase/src/main/java/in/spring4buddies/application/command/User.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/init-binder/sf-mvc-init-binder-annotation-usecase/src/main/java/in/spring4buddies/application/command/User.java
@@ -3,10 +3,24 @@ package in.spring4buddies.application.command;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
 public class User {
 
+	@NotEmpty(message = "UserName is empty")
 	private String name;
+
+	@DateTimeFormat(pattern = "MM/dd/yyyy")
+	@NotNull(message = "DateOfBirth is empty")
 	private Date dateOfBirth;
+
+	@NumberFormat(style = Style.NUMBER, pattern = "###,##,##,###.##")
+	@NotNull(message = "Salary is empty")
 	private BigDecimal salary;
 
 	public String getName() {
