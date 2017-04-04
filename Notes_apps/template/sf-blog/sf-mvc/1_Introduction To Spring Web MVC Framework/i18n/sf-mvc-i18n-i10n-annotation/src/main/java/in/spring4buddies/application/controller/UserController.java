b097c03d5be8df77bd3e0b7fa688494f545c85b7
Spring32 <spring4buddies@gmail.com>
2017-04-04 19:24:00 +0530

    Change-Id: I140ce1a45f9ef4ba17de5f0feef983e8426d4b32

--- Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/i18n/sf-mvc-i18n-i10n-annotation/src/main/java/in/spring4buddies/application/controller/UserController.java
+++ Notes_apps/template/sf-blog/sf-mvc/1_Introduction To Spring Web MVC Framework/i18n/sf-mvc-i18n-i10n-annotation/src/main/java/in/spring4buddies/application/controller/UserController.java
@@ -1,54 +1,22 @@
 package in.spring4buddies.application.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.validation.Valid;
+import in.spring4buddies.application.domain.User;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import in.spring4buddies.application.domain.User;
 
 @Controller
 public class UserController {
 
-	@ModelAttribute("countries")
-	public List<String> getCountries() {
-		List<String> countries = new ArrayList<>();
-		countries.add("Select");
-		countries.add("India");
-		countries.add("USA");
-		countries.add("UK");
-		return countries;
-	}
-
-	@ModelAttribute("communities")
-	public List<String> getCommunities() {
-		List<String> communities = new ArrayList<>();
-		communities.add("Spring");
-		communities.add("Hibernate");
-		communities.add("Struts");
-		return communities;
-	}
-
 	@RequestMapping("/userForm")
 	public String newUser(Model model) {
-		User user = new User();
-		model.addAttribute("user", user);
+		User user1 = new User();
+		User user1 = new User();
+		User user1 = new User();
+		
+		model.addAttribute("users", user);
 		return "userForm";
 	}
 
-	@RequestMapping(value = "/userForm", method = RequestMethod.POST)
-	public String saveUser(@Valid @ModelAttribute("user") User user, BindingResult result) {
-		if (result.hasErrors()) {
-			System.out.println(result);
-			return "userForm";
-		}
-		return "userSuccess";
-	}
 }
