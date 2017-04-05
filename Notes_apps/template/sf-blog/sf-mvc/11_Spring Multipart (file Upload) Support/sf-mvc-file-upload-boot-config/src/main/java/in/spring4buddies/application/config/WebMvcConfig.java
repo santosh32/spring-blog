031e6522708a6ca90d42cb1144fe774334bfb912
Santosh Spring <santosh.spring32@gmail.com>
2017-04-05 10:45:12 +0530

    Change-Id: Ibe82544a9f54ea9116b9ab961692aab5bcd65cf5

--- Notes_apps/template/sf-blog/sf-mvc/11_Spring Multipart (file Upload) Support/sf-mvc-file-upload-boot-config/src/main/java/in/spring4buddies/application/config/WebMvcConfig.java
+++ Notes_apps/template/sf-blog/sf-mvc/11_Spring Multipart (file Upload) Support/sf-mvc-file-upload-boot-config/src/main/java/in/spring4buddies/application/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "in.spring4buddies.application.controller")
+@ComponentScan(basePackages = { "in.spring4buddies.application.controller", "in.spring4buddies.application.config" })
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
