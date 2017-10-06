2e782971e73e28e627704ceb02e3b0106c718e0b
spring32 <spring4buddies@gmail.com>
2017-10-06 18:00:39 +0530

    Delete ProductClassRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/ProductClassRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.ProductClassRef;
-
-public interface ProductClassRefRepository extends PagingAndSortingRepository<ProductClassRef, String> {
-}
