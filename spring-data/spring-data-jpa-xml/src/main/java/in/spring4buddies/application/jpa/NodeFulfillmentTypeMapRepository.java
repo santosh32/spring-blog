487b3fa7304a74d060b9ef20621783b630509054
spring32 <spring4buddies@gmail.com>
2017-10-06 18:00:04 +0530

    Delete NodeFulfillmentTypeMapRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/NodeFulfillmentTypeMapRepository.java
+++
@@ -1,9 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.NodeFulfillmentTypeMap;
-import in.spring4buddies.application.model.NodeFulfillmentTypeMapPK;
-
-public interface NodeFulfillmentTypeMapRepository extends PagingAndSortingRepository<NodeFulfillmentTypeMap, NodeFulfillmentTypeMapPK> {
-}
