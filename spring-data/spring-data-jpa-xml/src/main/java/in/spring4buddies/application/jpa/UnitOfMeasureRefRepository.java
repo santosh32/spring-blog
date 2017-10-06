83d59bc9fd5b80ba2d1c63d65b180be00426a073
spring32 <spring4buddies@gmail.com>
2017-10-06 18:01:00 +0530

    Delete UnitOfMeasureRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/UnitOfMeasureRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.UnitOfMeasureRef;
-
-public interface UnitOfMeasureRefRepository extends PagingAndSortingRepository<UnitOfMeasureRef, String> {
-}
