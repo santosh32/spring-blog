b65902a216074c08ca1d320b68dec157fd3dbb2a
spring32 <spring4buddies@gmail.com>
2017-10-06 17:59:31 +0530

    Delete DayOfWeekRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/DayOfWeekRefRepository.java
+++
@@ -1,9 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.DayOfWeekRef;
-import in.spring4buddies.application.model.DayOfWeekRefPK;
-
-public interface DayOfWeekRefRepository extends PagingAndSortingRepository<DayOfWeekRef, DayOfWeekRefPK> {
-}
