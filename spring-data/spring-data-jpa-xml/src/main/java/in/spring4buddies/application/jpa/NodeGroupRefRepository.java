3d63165fa04b8a19e703fcd12e9cc2d0ad821e09
spring32 <spring4buddies@gmail.com>
2017-10-06 18:00:18 +0530

    Delete NodeGroupRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/NodeGroupRefRepository.java
+++
@@ -1,9 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.NodeGroupRef;
-import in.spring4buddies.application.model.NodeGroupRefPK;
-
-public interface NodeGroupRefRepository extends PagingAndSortingRepository<NodeGroupRef, NodeGroupRefPK> {
-}
