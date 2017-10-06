55e45abe779ac2446bc7ff8b45925dfb538e568a
spring32 <spring4buddies@gmail.com>
2017-10-06 17:59:55 +0530

    Delete InventoryOrganizationRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/InventoryOrganizationRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.InventoryOrganizationRef;
-
-public interface InventoryOrganizationRefRepository extends PagingAndSortingRepository<InventoryOrganizationRef, Integer> {
-}
