b48b5735d55c7d30001eeec9326bae0b88230677
spring32 <spring4buddies@gmail.com>
2017-10-06 17:59:23 +0530

    Delete CalendarRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/CalendarRefRepository.java
+++
@@ -1,9 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.CalendarRef;
-import in.spring4buddies.application.model.CalendarRefPK;
-
-public interface CalendarRefRepository extends PagingAndSortingRepository<CalendarRef, CalendarRefPK> {
-}
