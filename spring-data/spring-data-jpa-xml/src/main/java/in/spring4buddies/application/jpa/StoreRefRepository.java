2146a9871d7ff86851820284b2843b31d8432b55
spring32 <spring4buddies@gmail.com>
2017-10-06 18:00:54 +0530

    Delete StoreRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/StoreRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.StoreRef;
-
-public interface StoreRefRepository extends PagingAndSortingRepository<StoreRef, String> {
-}
