0e8e11b268b39072ce2bd43f14c13147ba2c0b2f
spring32 <spring4buddies@gmail.com>
2017-10-06 18:00:25 +0530

    Delete NodeRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/NodeRefRepository.java
+++
@@ -1,9 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.NodeRef;
-import in.spring4buddies.application.model.NodeRefPK;
-
-public interface NodeRefRepository extends PagingAndSortingRepository<NodeRef, NodeRefPK> {
-}
