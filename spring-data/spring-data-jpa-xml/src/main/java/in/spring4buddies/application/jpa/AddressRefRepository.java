22d1d1ca94c58b7b0d937b75ae506356e4831076
spring32 <spring4buddies@gmail.com>
2017-10-06 17:59:08 +0530

    Delete AddressRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/AddressRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.AddressRef;
-
-public interface AddressRefRepository extends PagingAndSortingRepository<AddressRef, Long> {
-}
