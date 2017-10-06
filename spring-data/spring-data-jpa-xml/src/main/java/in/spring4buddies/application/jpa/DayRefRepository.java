4ee780fc66507379a5ea0430bb17b3f246aa0cf5
spring32 <spring4buddies@gmail.com>
2017-10-06 17:59:38 +0530

    Delete DayRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/DayRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.DayRef;
-
-public interface DayRefRepository extends PagingAndSortingRepository<DayRef, Long> {
-}
