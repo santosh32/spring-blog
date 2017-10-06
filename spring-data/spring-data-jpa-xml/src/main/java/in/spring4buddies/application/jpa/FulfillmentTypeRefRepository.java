569b057e14d461551841186d9c487ee3a0e5d822
spring32 <spring4buddies@gmail.com>
2017-10-06 17:59:45 +0530

    Delete FulfillmentTypeRefRepository.java

--- spring-data/spring-data-jpa-xml/src/main/java/in/spring4buddies/application/jpa/FulfillmentTypeRefRepository.java
+++
@@ -1,8 +0,0 @@
-package in.spring4buddies.application.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import in.spring4buddies.application.model.FulfillmentTypeRef;
-
-public interface FulfillmentTypeRefRepository extends PagingAndSortingRepository<FulfillmentTypeRef, String> {
-}
