32cb02c926fc234de912f478ff87a9b2b83dcaf9
Spring32 <spring4buddies@gmail.com>
2017-11-29 20:41:22 +0530

    Change-Id: I8cdaf70c4400c4163ca815a937df3d27e7fc9026

--- Practise/src/sort/QuickSortEx.java
+++ Practise/src/sort/QuickSortEx.java
@@ -1,7 +1,7 @@
 package sort;
 
 public class QuickSortEx {
-	private int array[];
+	private int a[];
 	private int length;
 
 	public void sort(int[] inputArr) {
@@ -9,7 +9,7 @@ public class QuickSortEx {
 		if (inputArr == null || inputArr.length == 0) {
 			return;
 		}
-		this.array = inputArr;
+		this.a = inputArr;
 		length = inputArr.length;
 		quickSort(0, length - 1);
 	}
@@ -19,23 +19,19 @@ public class QuickSortEx {
 		int i = lowerIndex;
 		int j = higherIndex;
 		// calculate pivot number, I am taking pivot as middle index number
-		int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
+		int pivot = a[lowerIndex + (higherIndex - lowerIndex) / 2];
 		// Divide into two arrays
 		while (i <= j) {
-			/**
-			 * In each iteration, we will identify a number from left side which
-			 * is greater then the pivot value, and also we will identify a
-			 * number from right side which is less then the pivot value. Once
-			 * the search is done, then we exchange both numbers.
-			 */
-			while (array[i] < pivot) {
+			while (a[i] < pivot) {
 				i++;
 			}
-			while (array[j] > pivot) {
+			while (a[j] > pivot) {
 				j--;
 			}
 			if (i <= j) {
-				exchangeNumbers(i, j);
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
 				// move index to next position on both sides
 				i++;
 				j--;
@@ -48,16 +44,10 @@ public class QuickSortEx {
 			quickSort(i, higherIndex);
 	}
 
-	private void exchangeNumbers(int i, int j) {
-		int temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-
 	public static void main(String a[]) {
 
 		QuickSortEx sorter = new QuickSortEx();
-		int[] input = { 24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12 };
+		int[] input = { 11, 13, 5, 7, 3 };
 		sorter.sort(input);
 		for (int i : input) {
 			System.out.print(i);
